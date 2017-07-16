181a9332e2623e038a4fb7a561a2c8ca2cbb4b82
Andrew Schmelyun <schmelyun@gmail.com>
2017-07-16 00:54:24 -0400

    Dummy index page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     
-    public function index() {
-    
+    public function index()
+    {
+        return view('page.index');
     }
 
 }
