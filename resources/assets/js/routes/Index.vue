955ae6825da5a76f471a10b398f8c2b6d02d7707
Andrew Schmelyun <schmelyun@gmail.com>
2017-07-16 17:21:10 -0400

    Removal of old artifacts

--- resources/assets/js/routes/Index.vue
+++ resources/assets/js/routes/Index.vue
@@ -1,30 +1,5 @@
 <template>
     <div class="route-container">
-        <h3 class="mb-3">All Clients</h3>
-        <div class="row">
-            <div class="col col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <i class="fa fa-align-justify"></i>
-                        <strong>Client List</strong>
-                    </div>
-                    <div class="card-block">
-                        <table class="table table-bordered table-striped table-condensed">
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Monthly Allowance</th>
-                                <th>Status</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <h3 class="mb-3">Dashboard</h3>
     </div>
 </template>
--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,5 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'api', 'middleware' => 'auth', 'as' => 'api.'], function() {
 
-    Route::get('/websites', [
-        'as'    => 'websites.index',
-        'uses'  => 'WebsiteController@index'
-    ]);
 
 });
